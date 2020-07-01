da08ee166be9151f7961189808387bde67b2fe5b
Gabriel <gabrielnabas@id.uff.br>
2020-07-01 20:47:34 -0300

    delete-readme/build-history-comment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import GlobalStyle from './styles/global'
-import SessionComment from './pages/SessionComment'
+import GlobalStyle from './styles/global';
+import SessionComment from './pages/SessionComment';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './services/api'
+
 function App() {
   return (
     <>
-      <SessionComment></SessionComment>
-      <GlobalStyle></GlobalStyle>
-    </>
+      <ApolloProvider client={client}> 
+        <SessionComment/>
+        <GlobalStyle/>
+      </ApolloProvider>
+   </>
   );
 }
 
--- frontend/src/components/HistoryComment/index.js
+++ frontend/src/components/HistoryComment/index.js
@@ -1,14 +1,45 @@
 import React from 'react';
-
+import { useQuery } from '@apollo/react-hooks'
 import { Container } from './styles';
+import { gql }from 'apollo-boost';
+
+
+const GET_COMMENTS = gql`
+  query {
+    getComments {
+    id
+    name
+    content
+    createdAt
+    } 
+  }
+ `
+
 
 function HistoryComment() {
+  const { loading, error, data } = useQuery(GET_COMMENTS);
 
+  if( error ) return "DEU RUIM";
 
   return (
     <Container>
-    <h5>User: Paul </h5>
-    <p> Comment: lorem ipsulm cavan oekro keork kero</p>
+      <h1>Campo de Comentários</h1>
+      { loading ? ('Carregando...') : ( 
+          <section className="comments">
+            { data.getComments === 0 ? 
+              ('Não há nenhum comentário') : 
+              data.getComments.map(({ id, name, content }) => 
+                ( <div key={id}>
+                  <h3>User: {name} </h3>
+                  <p> Comment: {content}</p>
+                  </div>
+                )
+              )
+            }
+          </section>
+        )
+      }
+
     </Container>
   );
 }
