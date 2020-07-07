df4fb83de2f9493838a44c751bfc77468c14b8fb
Gabriel <gabrielnabas@id.uff.br>
2020-07-06 23:16:09 -0300

    .

--- client/src/pages/SessionComment/index.js
+++ client/src/pages/SessionComment/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import HistoryComments from '../../components/HistoryComment'
-import InputComment from '../../components/InputComment'
+import HistoryComments from '../../components/HistoryComment';
+import InputComment from '../../components/InputComment';
 
 import { Container } from './styles';
 
 function SessionComment() {
   return (
-    <Container>
-      <HistoryComments/>
-      <InputComment/>
-    </Container>
-  )
+    <>
+      <Container>
+        <HistoryComments />
+        <hr />
+        <InputComment />
+      </Container>
+    </>
+  );
 }
 
 export default SessionComment;
